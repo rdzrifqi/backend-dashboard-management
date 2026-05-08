638f838c7ee6aed24ad05ea55dce4b754a9325b3
rdzrifqi <rdzrifqi@gmail.com>
2026-05-08 13:23:46 +0700

    comment vendor bill

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { get_payments, get_total_payment, get_unique_customer, get_average_payment, get_customer_payment, get_top_customer, get_method_distribution,get_recent_payment } from '../controllers/paymentController.js';
-import { get_vendor_bill } from '../controllers/VendorBillController.js';
+// import { get_vendor_bill } from '../controllers/VendorBillController.js';
 const router=express.Router();
 
 router.get('/master', get_payments);
@@ -11,5 +11,5 @@ router.get('/customer_payment', get_customer_payment);
 router.get('/top_customer', get_top_customer);
 router.get('/method_distribution', get_method_distribution);
 router.get('/recent_payment', get_recent_payment);
-router.get('/vendor_bill', get_vendor_bill);
+// router.get('/vendor_bill', get_vendor_bill);
 export default router;
